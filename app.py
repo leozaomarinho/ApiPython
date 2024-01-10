54dab92aabcdbda2414baa2e7d846c515f9b3009
Leonardo Marinho <leowalkg6@gmail.com>
2024-01-09 22:59:50 -0300

    adicionando hoteis

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ from flask_restful import Resource,Api
 app = Flask(__name__)
 api = Api (app)
 
+hoteis = [
+    {'hotel_id': 'alpha',
+     'nome':'Alpha Hotel',
+     'estrelas': 4.3,
+     'diaria':420.34,
+     'cidade':'Rio de janeiro'},
+    
+     {'hotel_id': 'beira-mar',
+     'nome':'Beira-Mar Hotel',
+     'estrelas': 4.5,
+     'diaria':450.00,
+     'cidade':'Florianopolis'}, 
+     
+     {'hotel_id': 'paraiso',
+     'nome':'Paraiso Hotel',
+     'estrelas': 4.9,
+     'diaria':500.00,
+     'cidade':'Porto de Galinas'}
+]
+
 class Hoteis(Resource):
     def get(self):
-        return{'hoteis':'meus hoteis'}
+        return{'Hoteis':hoteis}
     #a library do flask converte o dict automaticamente para json
     
 api.add_resource(Hoteis,'/hoteis')

f9c35fe50359fad1b8d209588f785f0d7e2f804b
Leonardo Marinho <leowalkg6@gmail.com>
2024-01-17 21:55:24 -0300

    bug needs fix

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ api.add_resource(Hoteis,'/hoteis')
 api.add_resource(Hotel,'/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
-    from sql_alchemy import banco
+    from sqlalchemy import banco
     banco.init_app(app)
     app.run(debug=True)
 
--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,4 @@
-from sql_alchemy import banco
+from sqlalchemy import banco
 __tablename__ = 'hoteis'
     
 hotel_id = banco.Columns(banco.String, primary_key=True)
@@ -6,19 +6,20 @@ nome = banco.Columns(banco.String(80))
 estrelas = banco.Columns(banco.Float(precision=1))
 diaria = banco.Columns(banco.Float(precision=2))
 cidade = banco.Columns(banco.String(40))
-    
-def __init__(self,hotel_id, nome, estrelas,diaria,cidade):
-        self.hotel_id = hotel_id
-        self.nome = nome
-        self.estrelas = estrelas
-        self.diaria = diaria
-        self.cidade = cidade
-        
-def json(self):
-        return {
-            'hotel_id': self.hotel_id,
-            'nome':self.nome,
-            'estrelas': self.estrelas,
-            'diaria':self.diaria,
-            'cidade':self.cidade
-        }
+
+class Hotel(banco.Model):    
+    def __init__(self,hotel_id, nome, estrelas,diaria,cidade):
+            self.hotel_id = hotel_id
+            self.nome = nome
+            self.estrelas = estrelas
+            self.diaria = diaria
+            self.cidade = cidade
+            
+    def json(self):
+            return {
+                'hotel_id': self.hotel_id,
+                'nome':self.nome,
+                'estrelas': self.estrelas,
+                'diaria':self.diaria,
+                'cidade':self.cidade
+            }

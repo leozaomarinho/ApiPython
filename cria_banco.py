464a39547059a1b2b30e52d36566003cb78c759a
Leonardo Marinho <leowalkg6@gmail.com>
2024-01-17 21:30:54 -0300

    insert query

--- cria_banco.py
+++ cria_banco.py
@@ -6,7 +6,10 @@ cursor = connection.cursor()
 cria_tabela = 'CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY,\
     nome text, estrela real, diaria real, cidade text)'
     
+cria_hotel = "INSERT INTO hoteis VALUES('house','house beach',4.3,345.30,'Rio de janeiro')"    
 cursor.execute(cria_tabela)
+cursor.execute(cria_hotel)
 
 connection.commit()
 connection.close()
+
